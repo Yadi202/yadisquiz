7c2bebcd906c12c77489188f663a602fdffe5e42
Yadi202 <55799195+Yadi202@users.noreply.github.com>
2019-10-23 10:24:18 -0400

    Add files via upload

---
+++ quiz/questions.js
@@ -0,0 +1,91 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+
+    this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+
+
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+
+
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+// create questions here
+var questions = [
+  new Question("what is the name of my pet?", ["pen", "yadi jr","pookie", "connor"], "connor"),
+  new Question("whats my favorite color?", ["blue", "purple", "black", "red"], "black"),
+  new Question("what is 2+2?", ["4", "3","69", "1"], "4"),
+  new Question("what team is yadi in?", ["team a", "team b", "team c", "team b"], "team a"),
+  new Question("what school does yadi go to?", ["central", "ytech", "west york", "sinking springs"], "ytech")
+];
+
+// create quiz
+var quiz = new Quiz(questions);
+
+// display quiz
+populate();
